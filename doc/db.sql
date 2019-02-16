fb85465966f6a47dacd0aaac70c6cb5c9cc56217
mfslog <jerry.chau@qq.com>
2019-02-16 22:11:42 +0800

    feat:补全数据模型

--- doc/db.sql
+++ doc/db.sql
@@ -24,12 +24,14 @@ insert into t_slat(`id`,`slat`,`created`)values (1,'123456',1550297154);
 
 DROP TABLE IF EXISTS `t_category`;
 CREATE TABLE `t_category`(
-	  `id` BIGINT  auto_increment not null,
+    `id` BIGINT  auto_increment not null,
     `web_name` varchar(64) not null,
     `sys_name` varchar(64) not null,
     `parent_id` BIGINT not null default 0,
+    `priority` tinyint not null default 1 comment '展示顺序',
     `operator_type` tinyint not null comment '0 不可以修改,1 可以修改',
-	  `remark` varchar(200),
+    `state` tinyint not null default 1 comment '0 为激活，1激活',
+    `remark` varchar(200),
     `created` BIGINT not null,
     `updated` BIGINT not null,
     `operator_id` bigint not null,
@@ -41,14 +43,23 @@ CREATE TABLE `t_case`(
 	  `id` BIGINT auto_increment not null,
     `name` varchar(64) not null,
     `price` int not null default 0,
-    `type` int not null default 0 comment '0 个人，1 公司',
-    `style` int not null comment '风格类型',
-    `price_tag` int not null comment '造价区间标签',
+    `type` int not null default 0 comment ''0 个人，1 公司'',
     `owner_name` varchar(64) default null,
     `phone_number` varchar(20) default null,
     `add` varchar(200) default null,
     `created` bigint not null,
     `updated` bigint default null,
+    primary key (`id`)
+);
+
+drop table if exists `t_case_category`;
+create table `t_case_category`(
+	  `id` bigint auto_increment not null,
+    `case_id` bigint not null,
+    `category_id` bigint not null,
+    `created` bigint not null,
+    `updated` bigint default 0,
+    `state` tinyint not null default 1 comment ''0 失效, 1 有效'',
     primary key (`id`)
 );
 
